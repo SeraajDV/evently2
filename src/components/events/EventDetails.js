d775a0478c1d058cb21c7bfb9df127f21bbbdd74
SeraajDV <SeraajDV@users.noreply.github.com>
2019-10-02 10:34:59 +0200

    create signup and connect to firebase

--- src/components/events/EventDetails.js
+++ src/components/events/EventDetails.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom';
+
 
 const EventDetails = (props) => {
-    const { event } = props;
+    const { event, auth } = props;
+    if (!auth.uid) return <Redirect to='/signin' />;
     if (event) {
         return (
             <div className="container section event-details">
@@ -36,7 +39,8 @@ const mapStateToProps = (state, ownProps) => {
     const events = state.firestore.data.events;
     const event = events ? events[id] : null;
     return {
-        event: event
+        event: event,
+        auth: state.firebase.auth
     }
 }
 
